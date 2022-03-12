04381bcb4d401385981af2632223cf4177157a41
cheshsworld <94447348+cheshsworld@users.noreply.github.com>
2022-03-12 12:52:37 +0800

    added post project function and get individual project

--- crowdfunding/projects/models.py
+++ crowdfunding/projects/models.py
@@ -1,10 +1,9 @@
-from email.mime import image
-from turtle import title
 from django.db import models
 
 # Create your models here.
 class Project(models.Model):
     title = models.CharField(max_length=200)
+    description = models.TextField()
     goal = models.IntegerField()
     image = models.URLField()
     is_open = models.BooleanField()
