0515a5a3166f9c7145d04e7f4b4d4799c65054e2
cheshsworld <94447348+cheshsworld@users.noreply.github.com>
2022-04-23 11:50:53 +0800

    removed typo in url patterns

--- crowdfunding/crowdfunding/urls.py
+++ crowdfunding/crowdfunding/urls.py
@@ -37,6 +37,5 @@ urlpatterns = [
         path("schema/", SpectacularJSONAPIView.as_view(), name="schema"),
         path("schema/swagger-ui", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
         path("schema/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
-
     ]
 )
