47e048306c62db1b0cdf262e23a2ffe31e36363f
cheshsworld <94447348+cheshsworld@users.noreply.github.com>
2022-05-07 13:06:50 +0800

    added id to auth token

--- crowdfunding/users/urls.py
+++ crowdfunding/users/urls.py
@@ -5,7 +5,8 @@ from . import views
 urlpatterns = [
     path('', views.CustomUserList.as_view()),
     path('<int:pk>/', views.CustomUserDetail.as_view()),
-    path('register/', views.RegisterView.as_view()),   
+    path('register/', views.RegisterView.as_view()),
+    path('authenticate/', views.CustomObtainAuthToken.as_view()),   
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- crowdfunding/users/views.py
+++ crowdfunding/users/views.py
@@ -8,6 +8,17 @@ from rest_framework import status, generics, permissions
 from .models import CustomUser, Message
 from .serializers import CustomUserSerializer, RegisterSerializer,MessageSerializer, CustomUserDetailSerializer
 
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+
+
+class CustomObtainAuthToken(ObtainAuthToken):
+    def post(self, request, *args, **kwargs):
+        response = super(CustomObtainAuthToken, self).post(request, *args, **kwargs)
+        token = Token.objects.get(key=response.data['token'])
+        return Response({'token': token.key, 'id': token.user_id})
+
 
 
 class CustomUserList(APIView):
